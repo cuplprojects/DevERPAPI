79ca6036704c43192f884e68eceb81c55ef21d41
cuplprojects <shivom.chandrakala@gmail.com>
2025-03-24 12:30:48 +0530

    Qc Backend has been done

---
+++ ERPAPI/Model/QC.cs
@@ -0,0 +1,21 @@
+﻿namespace ERPAPI.Model
+{
+    public class QC
+    {
+        public int QCId { get; set; }
+        public bool? Language { get; set; }
+        public bool? MaxMarks { get; set; }
+        public bool? Duration { get; set; }
+        public bool? Status { get; set; }
+        public bool? TotalQuestions { get; set; }
+        public bool? SummationofMarksEqualsTotalMarks { get; set; }
+        public bool? StructureOfPaper { get; set; }
+        public bool? Series { get; set; }
+        public bool? A { get; set; }
+        public bool? B { get; set; }
+        public bool? C { get; set; }
+        public bool? D { get; set; }
+        public int ProjectId { get; set; }
+        public int QuantitySheetId { get; set; }
+    }
+}
